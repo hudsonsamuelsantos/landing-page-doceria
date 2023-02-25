3a6399e51ce80db51d9187b8bcf87ca98dbe60d2
Hudson Santos <hudsnss@gmail.com>
2023-02-25 00:46:42 -0300

    barra de navegação

---
+++ src/components/Nav.jsx
@@ -0,0 +1,23 @@
+export default function Nav() {
+    return (
+        <nav className="container">
+            <a className="logo" href="#">Line<span> Doceria Artesanal</span>.</a>
+
+            <div className="menu">
+                <ul className="grid">
+                    <li><a className="title" href="#home">Início</a></li>
+
+                    <li><a className="title" href="#about">Sobre</a></li>
+
+                    <li><a className="title" href="#menu">Cardápio</a></li>
+
+                    <li><a className="title" href="#contact">Contato</a></li>
+                </ul>
+            </div>
+
+            <div className="toggle icon-menu"></div>
+
+            <div className="toggle icon-close"></div>
+        </nav>
+    )
+}
---
+++ src/pages/_app.js
@@ -0,0 +1,6 @@
+import '@/styles/globals.css'
+import '@/assets/fonts/style.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
