c512c431a1a3f55a2851aa561f191a81ccaa31ea
Hudson Santos <hudsnss@gmail.com>
2023-03-08 20:50:37 -0300

    :construction: feat: Menu mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,17 @@ import { useEffect, useRef, useState } from "react"
 export default function Nav() {
 
   const nav = useRef()
+  const link = useRef()
   const [isScroll, setIsScroll] = useState(false)
+  const [isOpenMobileNav, setIsOpenMobileNav] = useState(false)
+
+  const toggleMobileNav = () => {
+    setIsOpenMobileNav(!isOpenMobileNav)
+  }
+
+  const closeMobileNav = () => {
+    setIsOpenMobileNav(false)
+  }
 
   function changeHeaderWhenScroll() {
 
@@ -22,26 +32,26 @@ export default function Nav() {
 
   return (
     <header id="header" className={isScroll ? "scroll" : ""}>
-      <nav className="container" ref={nav}>
+      <nav className={isOpenMobileNav ? "container show" : "container"} ref={nav}>
         <a className="logo" href="#">Line<span> Doceria Artesanal</span>.</a>
 
         <div className="menu">
           <ul className="grid">
-            <li><a className="title" href="#home">Início</a></li>
+            <li><a className="title" href="#home" onClick={closeMobileNav}>Início</a></li>
 
-            <li><a className="title" href="#about">Sobre</a></li>
+            <li><a className="title" href="#about" onClick={closeMobileNav}>Sobre</a></li>
 
-            <li><a className="title" href="#services">Serviços</a></li>
+            <li><a className="title" href="#services" onClick={closeMobileNav}>Serviços</a></li>
 
-            <li><a className="title" href="#menu">Cardápio</a></li>
+            <li><a className="title" href="#menu" onClick={closeMobileNav}>Cardápio</a></li>
 
-            <li><a className="title" href="#contact">Contato</a></li>
+            <li><a className="title" href="#contact" onClick={closeMobileNav}>Contato</a></li>
           </ul>
         </div>
 
-        <div className="toggle icon-menu"></div>
+        <div className="toggle icon-menu" onClick={toggleMobileNav}></div>
 
-        <div className="toggle icon-close"></div>
+        <div className="toggle icon-close" onClick={toggleMobileNav}></div>
       </nav>
     </header>
   )
